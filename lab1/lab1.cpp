1b2274251faf60300acf3215ad64fac7cc128c97
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-09-29 20:02:01 +0300

    Много мусора

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,12 +1,12 @@
-﻿// lab1.cpp: определяет точку входа для приложения.
-//
-
-#include "lab1.h"
+﻿#include "lab1.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+	_sleep(1000);
+	cout << "Bye CMake." << endl;
+	_sleep(1000);
 	return 0;
 }
