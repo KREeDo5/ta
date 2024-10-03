ddc4077b05262424c7c30dc73c787c72298a59f4
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-10-04 01:45:56 +0300

    форматирование, разбил на функции

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,175 +1,217 @@
 ﻿#include "lab1.h"
 
-struct Move
-{
-	string s;
-	string y;
+struct Move {
+    string s;
+    string y;
 };
 
-// Поиск хода в векторе
-vector<Move>::iterator FindMoveInVector(vector<Move>& vector, const Move& move)
+static vector<Move>::iterator FindMoveInVector(vector<Move>& vector, const Move& move)
 {
-	for (int i = 0; i < vector.size(); i++)
-	{
-		if ((vector[i].s == move.s) && (vector[i].y == move.y)) // сравнение двух состояний
-			return vector.begin() + i;
-	}
-	return vector.end();
+    return find_if(
+        vector.begin(),
+        vector.end(), 
+        [&](const Move& m)
+        {
+            return (m.s == move.s) && (m.y == move.y);
+        }
+    );
+}
+
+static int GetMoveIndex(const vector<Move>& list, const Move& move)
+{
+    for (int i = 0; i < list.size(); ++i)
+    {
+        if (list[i].s == move.s && list[i].y == move.y)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Чтение первой строки с выходными сигналами
+static vector<string> ReadSignals(istream& input)
+{
+    string tempStr;
+    vector<string> listOfY;
+    string item;
+
+    getline(input, tempStr);
+    size_t pos = tempStr.find(';');
+    if (pos != string::npos)
+    {
+        tempStr = tempStr.substr(pos + 1);  // Убираем всё до первого ';'
+    }
+
+    stringstream ss(tempStr);
+
+    while (getline(ss, item, ';'))
+    {
+        listOfY.push_back(item);
+    }
+    return listOfY;
+}
+
+// Чтение оставшейся части файла (состояния автомата)
+static vector<vector<Move>> ReadMealyTable(istream& input, vector<string>& listOfX, vector<Move>& listOfQ)
+{
+    vector<vector<Move>> table;
+    string tempStr;
+
+    while (getline(input, tempStr))
+    {
+        vector<Move> row;
+        string inputSignal;
+        string tempMove;
+        Move move;
+
+        stringstream rowStream(tempStr);
+        
+        getline(rowStream, inputSignal, ';');
+        listOfX.push_back(inputSignal);
+
+        while (getline(rowStream, tempMove, ';'))
+        {
+            size_t pos = tempMove.find('/');
+            move.s = tempMove.substr(0, pos);
+            move.y = tempMove.substr(pos + 1);
+            row.push_back(move);
+
+            if (FindMoveInVector(listOfQ, move) == listOfQ.end())
+            {
+                listOfQ.push_back(move);
+            }
+        }
+        table.push_back(row);
+    }
+
+    return table;
+}
+
+// Вывод таблицы автомата Мура
+static void WriteMooreTable(ostream& output, const vector<Move>& listOfQ, const vector<string>& listOfX, const vector<vector<Move>>& table, const vector<string>& listOfS)
+{
+    output << ";";
+    for (const auto& move : listOfQ)
+    {
+        output << move.y << ";";
+    }
+    output << endl;
+
+    output << ";";
+    for (size_t i = 0; i < listOfQ.size(); ++i)
+    {
+        output << "q" << i << ";";
+    }
+    output << endl;
+
+    for (size_t i = 0; i < listOfX.size(); ++i)
+    {
+        output << listOfX[i] << ";";
+        for (size_t j = 0; j < listOfQ.size(); ++j)
+        {
+            Move tempMove = table[i][
+                distance(
+                    listOfS.begin(), 
+                    find(
+                        listOfS.begin(), 
+                        listOfS.end(), 
+                        listOfQ[j].s
+                    )
+                )
+            ];
+            int index = GetMoveIndex(listOfQ, tempMove);
+            output << "q" << index << ";";
+        }
+        output << endl;
+    }
+}
+
+// Преобразование автомата Мили в Мура
+static void MealyToMoore(istream& input, ostream& output)
+{
+    vector<string> listOfS = ReadSignals(input);  // Чтение первой строки (состояния автомата)
+    vector<Move> listOfQ;
+    vector<string> listOfX;
+    vector<vector<Move>> table = ReadMealyTable(input, listOfX, listOfQ);  // Чтение таблицы автомата
+
+    // Сортировка состояний автомата Мура
+    sort(
+        listOfQ.begin(), 
+        listOfQ.end(), 
+        [](const Move& a, const Move& b)
+        {
+            return a.s < b.s;
+        }
+    );
+
+    WriteMooreTable(output, listOfQ, listOfX, table, listOfS);  // Запись таблицы автомата Мура
+}
+
+// Преобразование автомата Мура в Мили
+static void MooreToMealy(istream& input, ostream& output)
+{
+    string tempStr;
+
+    auto listOfY = ReadSignals(input);
+    getline(input, tempStr);
+    output << tempStr << endl;  // вывод первой строки _|a0|a1|a2|a3|a4|
+
+    while (getline(input, tempStr))
+    {
+        size_t pos = 0;
+        int count = 0;
+        size_t yIndex = 0;
+
+        while ((pos = tempStr.find(';', pos)) != string::npos)
+        {
+            count++;
+            if (count > 1 && yIndex < listOfY.size())
+            {
+                tempStr.insert(pos, "/" + listOfY[yIndex]);
+                pos += listOfY[yIndex].length() + 1;
+                yIndex++;
+            }
+            pos++;
+        }
+
+        if (yIndex < listOfY.size())
+        {
+            tempStr += "/" + listOfY[yIndex];  // Добавляем оставшийся элемент перед концом строки
+        }
+
+        output << tempStr << endl;
+    }
 }
 
 int main(int argc, char* argv[])
 {
-	string mode = argv[1];
-	if (mode == "moore-to-mealy")
-	{
-		ifstream input(argv[2]);
-		ofstream output(argv[3]);
-		map<string, string> moves;
-		string tempStr;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-		getline(input, tempStr);
+    if (argc != 4)
+    {
+        cerr << argv[0] << " <mode> <input_file> <output_file>" << endl;
+        return 1;
+    }
 
-		size_t pos = tempStr.find(';');
-		if (pos != string::npos)
-		{
-			tempStr = tempStr.substr(pos + 1);  // Убираем всё до первого ';'
-		}
+    string mode = argv[1];
+    ifstream input(argv[2]);
+    ofstream output(argv[3]);
 
-		vector<string> listOfY;
-		stringstream ss(tempStr);
-		string item;
+    if (mode == "moore-to-mealy")
+    {
+        MooreToMealy(input, output);
+    }
+    else if (mode == "mealy-to-moore")
+    {
+        MealyToMoore(input, output);
+    }
+    else
+    {
+        cerr << "Некорректный режим. Используйте mealy-to-moore или moore-to-mealy." << endl;
+        return 1;
+    }
 
-		while (getline(ss, item, ';'))
-		{
-			listOfY.push_back(item);
-		}
-
-		getline(input, tempStr);
-		output << tempStr << endl; //вывод первой строки _|a0|a1|a2|a3|a4|
-
-		while (getline(input, tempStr))
-		{
-			size_t pos = 0;
-			int count = 0; // Счетчик для отслеживания количества ';'
-			size_t yIndex = 0;
-
-			while ((pos = tempStr.find(';', pos)) != string::npos)
-			{
-				count++;
-				// Если это второй или последующий ';' и есть элементы в listOfY
-				if (count > 1 && yIndex < listOfY.size())
-				{
-					tempStr.insert(pos, "/" + listOfY[yIndex]);
-					pos += listOfY[yIndex].length() + 1;
-					yIndex++;
-				}
-				pos++;
-			}
-
-			if (yIndex < listOfY.size()) {
-				tempStr += "/" + listOfY[yIndex]; // Добавляем оставшийся элемент перед концом строки
-			}
-
-			output << tempStr << endl;
-		}
-
-		input.close();
-		output.close();
-		return 0;
-	}
-	else if (mode == "mealy-to-moore")
-	{
-        ifstream input(argv[2]);
-        string tempStr;
-        getline(input, tempStr);
-
-		size_t pos = tempStr.find(';');
-		if (pos != string::npos) 
-		{
-			tempStr = tempStr.substr(pos + 1);  // Убираем всё до первого ';'
-		}
-
-        vector<string> listOfS;
-        stringstream ss(tempStr);
-        string state;
-        while (getline(ss, state, ';')) 
-		{
-            listOfS.push_back(state);  // Храним состояния
-        }
-
-        vector<vector<Move>> table;
-        vector<string> listOfX;
-        vector<Move> listOfQ;  // Список состояний автомата Мура
-        while (getline(input, tempStr)) 
-		{
-            vector<Move> string;
-            stringstream rowStream(tempStr);
-            std::string inputSignal;
-            getline(rowStream, inputSignal, ';');
-            listOfX.push_back(inputSignal);
-
-            std::string tempMove;
-            Move move;
-            while (getline(rowStream, tempMove, ';'))
-			{
-                size_t pos = tempMove.find('/');
-                move.s = tempMove.substr(0, pos);
-                move.y = tempMove.substr(pos + 1);
-                string.push_back(move);
-
-                if (FindMoveInVector(listOfQ, move) == listOfQ.end())
-				{
-                    listOfQ.push_back(move);  // Добавляем уникальные состояния
-                }
-            }
-            table.push_back(string);
-        }
-        input.close();
-
-        // Сортировка состояний автомата Мура
-        sort(
-			listOfQ.begin(), 
-			listOfQ.end(), 
-			[](const Move& a, const Move& b) 
-			{
-				return a.s < b.s;
-			}
-		);
-
-        ofstream output(argv[3]);
- 
-		// Вывод таблицы Мура
-        output << ";";
-        for (const auto& move : listOfQ)
-		{
-            output << move.y << ";";
-        }
-        output << endl;
-
-        output << ";";
-        for (size_t i = 0; i < listOfQ.size(); ++i)
-		{
-            output << "q" << i << ";";
-        }
-        output << endl;
-
-        for (size_t i = 0; i < listOfX.size(); ++i)
-		{
-            output << listOfX[i] << ";";
-            for (size_t j = 0; j < listOfQ.size(); ++j)
-			{
-                Move tempMove = table[i][distance(listOfS.begin(),
-                    find(listOfS.begin(), listOfS.end(), listOfQ[j].s))];
-                output << "q" << distance(listOfQ.begin(), FindMoveInVector(listOfQ, tempMove)) << ";";
-            }
-            output << endl;
-        }
-
-        output.close();
-	}
-	else
-	{
-		cerr << "Некорректный режим. Используйте mealy-to-moore или moore-to-mealy." << endl;
-		return 1;
-	}
+    return 0;
 }
