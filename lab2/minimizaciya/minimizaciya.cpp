f686f9aae203833674bfff20ec5f1503ed1d2bc6
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-10-27 18:21:08 +0300

    init

---
+++ lab2/minimizaciya/minimizaciya.cpp
@@ -0,0 +1,12 @@
+﻿// minimizaciya.cpp: определяет точку входа для приложения.
+//
+
+#include "minimizaciya.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
