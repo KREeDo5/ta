c041237f1b9f2b5eed8e5bf0339f86f63ee66d35
Dmitrii Liashko <mitya.lyashko.02@mail.ru>
2024-10-27 18:50:31 +0300

    Перенёс из прошлой ЛР чтение входных данных Исправил заголовочный файл

--- lab2/minimizaciya/minimizaciya.cpp
+++ lab2/minimizaciya/minimizaciya.cpp
@@ -1,11 +1,45 @@
-﻿#include <windows.h>
-#include "minimizaciya.h"
+﻿#include "minimizaciya.h"
 
-using namespace std;
-
-int main()
+static void MooreMin(istream& input, ostream& output)
 {
-	cout << "Hello CMake." << endl;
+	cout << "Минимизация Мура";
 	Sleep(1000);
+}
+
+static void MealyMin(istream& input, ostream& output)
+{
+    cout << "Минимизация Мили";
+    Sleep(1000);
+}
+
+int main(int argc, char* argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argc != 4)
+	{
+		cerr << argv[0] << " <mode> <input_file>.csv <output_file>.csv" << endl;
+		return 1;
+	}
+
+	string mode = argv[1];
+	ifstream input(argv[2]);
+	ofstream output(argv[3]);
+
+	if (mode == "moore")
+	{
+		MooreMin(input, output);
+	}
+	else if (mode == "mealy")
+	{
+		MealyMin(input, output);
+	}
+	else
+	{
+		cerr << "Некорректный режим. Введите mealy или moore." << endl;
+		return 1;
+	}
+
 	return 0;
 }
--- lab2/minimizaciya/minimizaciya.h
+++ lab2/minimizaciya/minimizaciya.h
@@ -1,8 +1,7 @@
-﻿// minimizaciya.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <fstream>
+#include <Windows.h>
 
-// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+using namespace std;
