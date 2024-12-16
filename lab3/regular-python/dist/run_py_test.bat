77795cbf0d0dccf662578d44447e30ed55eaf458
DESKTOP-E6U75VE\dimal <dmitrii.liashko>
2024-12-16 12:34:39 +0300

    пересобрал ехе

--- lab3/regular-python/dist/run_py_test.bat
+++ lab3/regular-python/dist/run_py_test.bat
@@ -19,5 +19,25 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
+echo _______________________________
+echo Run test 3...
+
+main.exe tests/right2.txt tests/output-right2.csv
+if %errorlevel% neq 0 (
+    echo Test failed: tests/right2.txt
+    pause
+    exit /b %errorlevel%
+)
+
+echo _______________________________
+echo Run test 4...
+
+main.exe tests/inputRight1.txt tests/output-right3.csv
+if %errorlevel% neq 0 (
+    echo Test failed: tests/inputRight1.txt
+    pause
+    exit /b %errorlevel%
+)
+
 echo All tests completed successfully.
 pause
