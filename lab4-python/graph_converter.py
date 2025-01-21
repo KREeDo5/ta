ab0eb1ba39ac9fd30003f2d56dd73bc632d21c36
Dmitrii Liashko <dmitrii.liashko>
2025-01-21 12:08:09 +0300

    комменты

--- lab4-python/graph_converter.py
+++ lab4-python/graph_converter.py
@@ -3,14 +3,13 @@ import networkx as netx
 def convert_to_dfa(nfa):
     dfa = netx.DiGraph()
 
-    start_state = nfa.graph['start']  # Получаем начальное состояние NFA
+    start_state = nfa.graph['start']  # Получаю начальное состояние NFA
     print("начальное состояние:", start_state)
 
-    epsilon_closure = get_epsilon_closure(nfa, {start_state}) # Вычисляем ε-замыкание начального состояния
+    epsilon_closure = get_epsilon_closure(nfa, {start_state}) # Нахожу ε-замыкание начального состояния
     print("ε-замыкание начального состояния:", epsilon_closure)
 
-    # Используем очередь для обработки новых состояний DFA
-    state_queue = [frozenset(epsilon_closure)]
+    state_queue = [frozenset(epsilon_closure)] #очередь для обработки новых состояний DFA
     dfa_states = {frozenset(epsilon_closure): 'S0'}  # отображение набора NFA-состояний в DFA-состояния
     dfa.graph['start'] = 'S0'
     dfa_counter = 1
@@ -23,37 +22,38 @@ def convert_to_dfa(nfa):
 
         print("current_dfa_state:", current_dfa_state)
 
-        # Проверяем финальность текущего состояния
+        # Проверяю финальность текущего состояния
         is_final = any(nfa.nodes[state].get('is_final', False) for state in current_set)
         dfa.add_node(current_dfa_state, is_final=is_final)
 
         print(f"Текущее состояние DFA: {current_dfa_state}, включает NFA состояния: {current_set}, финальное: {is_final}")
 
-        # Получаем все возможные сигналы (кроме ε)
+        # Получаю все возможные входные символы (кроме ε)
         signals = set(edge_data['in_signal'] for state in current_set
                       for _, _, edge_data in nfa.out_edges(state, data=True)
                       if edge_data['in_signal'] != 'ε')
 
         for signal in signals:
-            # Собираем все состояния, достижимые по сигналу
+            # Собираю переходы, достижимые из текущего состояния по входному сигналу
             next_states = set()
             for state in current_set:
                 for _, to_state, edge_data in nfa.out_edges(state, data=True):
                     if edge_data['in_signal'] == signal:
                         next_states.add(to_state)
 
-            # Рассчитываем ε-замыкание для полученных состояний
+            # Рассчитываю ε-замыкание для полученных состояний
             epsilon_closure_next = get_epsilon_closure(nfa, next_states)
             print("ε-замыкание полученного состояния:", epsilon_closure_next)
             next_set = frozenset(epsilon_closure_next)
 
+            #Если новое состояние еще не добавлялось
             if next_set not in dfa_states:
                 dfa_states[next_set] = f'S{dfa_counter}'
                 state_queue.append(next_set)
                 print(f"Новое состояние DFA: {dfa_states[next_set]} с ε-замыканием: {epsilon_closure_next}")
                 dfa_counter += 1
 
-            # Добавляем переход в DFA
+            # Добавляю переход в DFA
             dfa.add_edge(current_dfa_state, dfa_states[next_set], in_signal=signal)
             print(f"Добавлен переход: {current_dfa_state} --{signal}--> {dfa_states[next_set]}")
 
