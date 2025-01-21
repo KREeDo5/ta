6a58413817ed38591dfa566f8d74056df24108d2
Dmitrii Liashko <dmitrii.liashko>
2025-01-21 22:20:55 +0300

    проверки на зацикливание

--- lab5-python/main.py
+++ lab5-python/main.py
@@ -20,11 +20,37 @@ def split_regex(regex):
 
     return components
 
+def process_component(component):
+    if component.endswith('*') or component.endswith('+'):
+        operator = component[-1]
+        sub_component = component[:-1]
+        if sub_component.startswith('(') and sub_component.endswith(')'):
+            sub_components = split_regex(sub_component[1:-1])
+            for sub_component in sub_components:
+                if operator == '*':
+                    print(f"{sub_component} - зацикливание в состоянии и e-переход перед этим состоянием")
+                elif operator == '+':
+                    print(f"{sub_component} - зацикливание в состоянии и один обязательный переход перед этим состоянием")
+        else:
+            if operator == '*':
+                print(f"{sub_component} - зацикливание в состоянии и e-переход перед этим состоянием")
+            elif operator == '+':
+                print(f"{sub_component} - зацикливание в состоянии и один обязательный переход перед этим состоянием")
+    else:
+        if component.startswith('(') and component.endswith(')'):
+            sub_components = split_regex(component[1:-1])
+            for sub_component in sub_components:
+                process_component(sub_component)
+        else:
+            sub_components = split_regex(component)
+            for sub_component in sub_components:
+                print(sub_component)
+
 def main():
-    regex = "x|y*(x|y*)+|ab(x|y*)|(x|a*)(x|y*)"
+    regex = "xy*(x|y*)|(ab(x|y*)*)|((x|a*)(x|y*))+"
     components = split_regex(regex)
     for component in components:
-        print(component)
+        process_component(component)
 
 if __name__ == "__main__":
     main()
