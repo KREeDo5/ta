089f3118ef493bbb955578bac7af5af004ad1323
Dmitrii Liashko <dmitrii.liashko>
2025-01-26 04:15:23 +0300

    Вывод в 2 файла

--- lw6-lexer/main.py
+++ lw6-lexer/main.py
@@ -5,16 +5,22 @@ def main(input_filename, output_filename):
     with open(input_filename, 'r', encoding='utf-8') as file:
         text = file.read()
 
+    output_code_filename =  './code_output.txt'   
+
     lx = Lexer()
     lx.input(text)
 
-    try:
-        with open(output_filename, 'w', encoding='utf-8') as output_file:
-            for token in lx.tokens():
+    tokenList = list(lx.tokens())
+
+    with open(output_code_filename, 'w', encoding='utf-8') as output_code_file:
+        for token in tokenList:
+            output_code_file.write(str(token) + '\n')
+
+    with open(output_filename, 'w', encoding='utf-8') as output_file:
+        for token in tokenList:
+            if token.type != 'comment':
                 output_file.write(str(token) + '\n')
-    except LexerError as errorString:
-        with open(output_filename, 'w', encoding='utf-8') as output_file:
-            output_file.write(str(errorString) + '\n')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
